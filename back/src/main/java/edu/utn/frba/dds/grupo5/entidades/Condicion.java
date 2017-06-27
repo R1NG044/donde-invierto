da2b0be96e01339eb8bfa9e3ff5c04cfb7a40275
SolanaJacquemin <jacquemin.solana@gmail.com>
2017-06-26 22:14:07 -0300

    agrega algunos methodos

--- back/src/main/java/edu/utn/frba/dds/grupo5/entidades/Condicion.java
+++ back/src/main/java/edu/utn/frba/dds/grupo5/entidades/Condicion.java
@@ -1,5 +1,18 @@
 package edu.utn.frba.dds.grupo5.entidades;
 
-public class Condicion {
+import java.util.List;
 
+import edu.utn.frba.dds.grupo5.util.Util;
+
+public class Condicion {
+	String indicador;
+	public Boolean antiguedad(Empresa empresa, Integer cant){
+		return empresa.antiguedad() > cant;
+	}
+	
+	public List<Empresa> ordenarPorAntiguedad(List<Empresa> empresas, Integer cant){
+		//List<Empresa> newEm= Util.filterByPredicate(empresas.getEmpresas(),e -> antiguedad(e,cant));
+		//ordenar(newEm);
+		//return newEm;
+	}
 }
--- back/src/main/java/edu/utn/frba/dds/grupo5/entidades/CondicionConstante.java
+++ back/src/main/java/edu/utn/frba/dds/grupo5/entidades/CondicionConstante.java
@@ -1,5 +1,10 @@
 package edu.utn.frba.dds.grupo5.entidades;
 
+import java.util.List;
+
 public class CondicionConstante {
+	String indicador;
+	int rango;
+	public List<Empresa>  empresa;
 
 }
