6e8a477fdbe42fdf16c9e0bcd38afdc8d4910670
Janice <janicebahiana@gmail.com>
2017-08-27 13:03:05 -0300

    "Se agrega mapeo de indicadores"

--- back/src/main/java/edu/utn/frba/dds/grupo5/entidades/Indicador.java
+++ back/src/main/java/edu/utn/frba/dds/grupo5/entidades/Indicador.java
@@ -3,13 +3,32 @@ package edu.utn.frba.dds.grupo5.entidades;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Indicador {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Table(name="di_indicador")
+@Entity
+public class Indicador extends Persistent{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8156980671136462943L;
 	private String expression;
 	private List<Indicador> indicadores;
 	private List<Cuenta> cuentas;
 	private String nombre;
 	
+	public Indicador (){
+		
+	}
+	@Column(name="cu_descripcion",length=200,nullable=false)
 	public String getExpression() {
 		return expression;
 	}
@@ -17,7 +36,8 @@ public class Indicador {
 	public void setExpression(String expression) {
 		this.expression = expression;
 	}
-	
+	@OneToMany(cascade={CascadeType.ALL},fetch=FetchType.LAZY,orphanRemoval=true)
+	@JoinColumn(name="ind_ind_oid",nullable=true,foreignKey=@ForeignKey(name="fk_ind_ind_oid"))
 	public List<Indicador> getIndicadores() {
 		if(indicadores == null)
 			indicadores = new ArrayList<Indicador>();
@@ -27,7 +47,8 @@ public class Indicador {
 	public void setIndicadores(List<Indicador> indicadores) {
 		this.indicadores = indicadores;
 	}
-
+	@OneToMany(cascade={CascadeType.ALL},fetch=FetchType.LAZY,orphanRemoval=true)
+	@JoinColumn(name="ind_cue_oid",nullable=true,foreignKey=@ForeignKey(name="fk_ind_cue_oid"))
 	public List<Cuenta> getCuentas() {
 		if(cuentas == null)
 			cuentas = new ArrayList<Cuenta>();
@@ -37,7 +58,7 @@ public class Indicador {
 	public void setCuentas(List<Cuenta> cuentas) {
 		this.cuentas = cuentas;
 	}
-
+	@Column(name="ce_valor",nullable=false,scale=2)
 	public String getNombre() {
 		return nombre;
 	}
