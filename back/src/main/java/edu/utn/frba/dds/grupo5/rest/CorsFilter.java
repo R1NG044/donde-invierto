652eaa7d7172f57bb0c055fa1faebf6d3a8b36c9
Martin Picollo <mpicollo.utn@gmail.com>
2017-10-11 09:16:52 -0300

    Update CorsFilter.java

--- back/src/main/java/edu/utn/frba/dds/grupo5/rest/CorsFilter.java
+++ back/src/main/java/edu/utn/frba/dds/grupo5/rest/CorsFilter.java
@@ -14,7 +14,7 @@ public class CorsFilter extends OncePerRequestFilter {
                                     HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
             response.addHeader("Access-Control-Allow-Origin", "*");
-            response.addHeader("Access-Control-Allow-Methods", "POST,PUT,GET,DELETE");
+            response.addHeader("Access-Control-Allow-Methods", "POST,PUT,GET,DELETE,OPTIONS");
             response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             filterChain.doFilter(request, response);
     }
